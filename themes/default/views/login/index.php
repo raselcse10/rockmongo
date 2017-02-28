6020779014ffcd0cdfa427ad37f2a2ced2a12d50
raselranaibt <rasel@bluebd.com>
2017-02-28 16:28:27 +0600

    Translate to Bengali

--- themes/default/views/login/index.php
+++ themes/default/views/login/index.php
@@ -30,61 +30,107 @@ function r_show_more_options(btn) {
 }
 </script>
 
-<div style="padding:10px;margin:200px auto;width:300px;border:1px #ccc solid">
-<?php if (isset($message)):?><p class="error"><?php h($message); ?></p><?php endif;?>
-	<form method="post">
-	<input type="hidden" name="more" id="show_more_options" value="<?php echo $moreOptions; ?>"/>
-	<table>
-		<tr>
-			<td colspan="2" style="background:#eee"><strong>Log-in:</strong></td>
-		</tr>
-		<tr>
-			<td width="100">Host</td>
-			<td><?php render_select_hosts("host", $hostIndex); ?></td>
-		</tr>
-		<tr>
-			<td nowrap><?php hm("username"); ?>:</td>
-			<td><input type="text" name="username" value="<?php echo $username;?>" style="width:150px"/></td>
-		</tr>
-		<tr>
-			<td><?php hm("password"); ?>:</td>
-			<td><input type="password" name="password" style="width:150px"/></td>
-		</tr>
-		<tr>
-			<td colspan="2" style="background:#eee"><strong>Non-Admin users:</strong></td>
-		</tr>
-		<tr>
-			<td valign="top">DB Name(s):</td>
-			<td><input type="text" name="db" style="width:150px" value="<?php h($db); ?>"/><br/></td>
-		</tr>
-		<tr>
-			<td colspan="2"><a href="#" onclick="return r_show_more_options(this)" id="more_options_btn">More &raquo;</a></td>
-		</tr>
-		<tbody style="display:none" id="more_options">
-		<tr>
-			<td><?php hm("language"); ?>:</td>
-			<td><select name="lang" style="width:150px">
-			<?php foreach ($languages as $code => $lang):?>
-			<option value="<?php h($code);?>" <?php if(x("lang") == $code || (x("lang") ==""&&__LANG__==$code)): ?>selected="selected"<?php endif;?> ><?php h($lang); ?></option>
-			<?php endforeach;?>
-			</select></td>
-		</tr>
-		<tr>
-			<td><?php hm("alive"); ?>:</td>
-			<td>
-			<select name="expire" style="width:150px">
-			<?php foreach ($expires as $long => $name):?>
-			<option value="<?php h($long);?>"><?php h($name);?></option>
-			<?php endforeach;?>
-			</select>
-			</td>
-		</tr>
-		</tbody>
-		<tr>
-			<td colspan="2" align="center"><input type="submit" value="<?php hm("loginandrock"); ?>"/></td>
-		</tr>
-	</table>
-	</form>
+
+
+<div class="container login-panel">
 	
-	<?php hm("rockmongocredits") ?>
+	<fieldset>
+		<h1>Welcome to MongoDB Administration</h1>
+	</fieldset>
+
+	<fieldset>
+		<legend>
+			Log in
+		</legend>
+		<!-- 
+		<h1>Welcome to MongoDB Administration</h1>
+		<div class="dotted"></div> -->
+
+		<?php if (isset($message)):?> 
+			<div class="errorMsg">
+				
+				<p><?php h($message); ?> </p>
+				
+			</div>
+		<?php endif;?>
+
+		<form method="post" style="display: block;" class="login">
+			<input type="hidden" name="more" id="show_more_options" value="<?php echo $moreOptions; ?>"/>
+			
+			<div class="item">
+				<label for="host">Host:</label>
+				<?php render_select_hosts("host", $hostIndex); ?>
+			</div>
+
+			<div class="item">
+
+				<label for="usrename"><?php hm("username"); ?>:</label>
+				<input type="text" name="username" value="<?php echo $username;?>"  size="24" class="textfield"/>
+			
+			</div>
+
+			<div class="item">
+
+				<label for="password"><?php hm("password"); ?>:</label>
+				<input type="password" name="password" size="24" class="textfield"/>
+
+			</div>
+
+			<div class="item">
+				
+				<p> <strong>Non-Admin users:</strong> </p>
+				
+			</div>
+
+			<div class="item">
+
+				<label for="dbname">DB Name(s)</label>
+				<input type="text" name="db" value="<?php h($db); ?>" size="24" class="textfield"/>
+
+			</div>
+			
+			<div class="item">
+				<a href="#" onclick="return r_show_more_options(this)" id="more_options_btn">More &raquo;</a>
+			</div>
+			
+			<div style="display:none" id="more_options">
+				
+				<div class="item">
+				
+					<label for="language"><?php hm("language"); ?>:</label>
+					<select name="lang" class="select-list">
+						<?php foreach ($languages as $code => $lang):?>
+						<option value="<?php h($code);?>" <?php if(x("lang") == $code || (x("lang") ==""&&__LANG__==$code)): ?>selected="selected"<?php endif;?> ><?php h($lang); ?></option>
+						<?php endforeach;?>
+					</select>
+
+				</div>
+
+				<div class="item">
+				
+					<label for="language"><?php hm("alive"); ?>:</label>
+					<select name="expire" class="select-list">
+						<?php foreach ($expires as $long => $name):?>
+						<option value="<?php h($long);?>"><?php h($name);?></option>
+						<?php endforeach;?>
+					</select>
+
+				</div>
+
+			</div>
+			
+			<div class="item">
+				<button class="login-button"><?php hm("loginandrock"); ?></button>
+				<!-- <input type="submit" value="<?php hm("loginandrock"); ?>"/> -->
+			</div>
+
+		
+	<fieldset>	
+	
+			
+		</form>
+
+			
+		<?php hm("rockmongocredits") ?>
+
 </div>
